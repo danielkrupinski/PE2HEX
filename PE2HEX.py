dd76e4e017e29cb5a443fc34d9a04e585f592cd9
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-08 13:16:29 +0200

    Check argument count

--- PE2HEX.py
+++ PE2HEX.py
@@ -1,8 +1,12 @@
 import sys
 
+if len(sys.argv) < 2:
+    print('Usage: PE2HEX input_filename [output_filename]\nExample: PE2HEX input.dll output.txt')
+    exit(1)
+
 file = bytearray(open(sys.argv[1], 'rb').read())
 
-output = open(sys.argv[2], 'w')
+output = open(sys.argv[2] if len(sys.argv) >= 3 else 'output.txt', 'w')
 output.write('static const uint8_t binary[]{\n')
 
 byteCount = 0
