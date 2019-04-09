a2048d754eecd4fc24ce0a45d17f471f94c7745d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-09 12:05:22 +0200

    Simplify loop

--- PE2HEX.py
+++ PE2HEX.py
@@ -9,13 +9,7 @@ file = bytearray(open(sys.argv[1], 'rb').read())
 output = open(sys.argv[2] if len(sys.argv) >= 3 else 'output.txt', 'w')
 output.write('static const uint8_t binary[]{\n')
 
-byteCount = 0
-
-for byte in file:
-    output.write(f'{byte:#0{4}x}, ')
-    byteCount += 1
-    if byteCount == 16:
-        output.write('\n')
-        byteCount = 0
+for count, byte in enumerate(file, start=1):
+    output.write(f'{byte:#0{4}x}, ' + ('\n' if not count % 16 else ''))
 
 output.write('};')
