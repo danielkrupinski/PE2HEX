804fd89e1949b2ea3aa88ff373955d91a0e7afc5
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-12 13:09:30 +0200

    Pass unnamed argument

--- PE2HEX.py
+++ PE2HEX.py
@@ -9,7 +9,7 @@ file = bytearray(open(sys.argv[1], 'rb').read())
 output = open(sys.argv[2] if len(sys.argv) >= 3 else 'output.txt', 'w')
 output.write('static const uint8_t binary[]{\n')
 
-for count, byte in enumerate(file, start=1):
+for count, byte in enumerate(file, 1):
     output.write(f'{byte:#0{4}x}, ' + ('\n' if not count % 16 else ''))
 
 output.write('};')
