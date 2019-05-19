cb01ad8965f3140e2c1c7e4facaa1c36839f315d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-19 12:18:53 +0200

    Update output syntax for compatibility with C

--- PE2HEX.py
+++ PE2HEX.py
@@ -7,7 +7,7 @@ if len(sys.argv) < 2:
 file = bytearray(open(sys.argv[1], 'rb').read())
 
 output = open(sys.argv[2] if len(sys.argv) >= 3 else 'output.txt', 'w')
-output.write('static const uint8_t binary[]{\n')
+output.write('static const uint8_t binary[] = {\n')
 
 for count, byte in enumerate(file, 1):
     output.write(f'{byte:#0{4}x},' + ('\n' if not count % 16 else ' '))
