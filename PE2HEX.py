a3d41f651d6d8a77ce4d2199478365d353dae636
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-22 20:06:01 +0200

    Make output language independent

--- PE2HEX.py
+++ PE2HEX.py
@@ -6,10 +6,6 @@ if len(sys.argv) < 2:
 
 file = bytearray(open(sys.argv[1], 'rb').read())
 
-output = open(sys.argv[2] if len(sys.argv) >= 3 else 'output.txt', 'w')
-output.write('static const uint8_t binary[] = {\n')
-
-for count, byte in enumerate(file, 1):
-    output.write(f'{byte:#0{4}x},' + ('\n' if not count % 16 else ' '))
-
-output.write('};')
+with open(sys.argv[2] if len(sys.argv) >= 3 else 'output.txt', 'w') as output:
+    for count, byte in enumerate(file, 1):
+        output.write(f'{byte:#0{4}x},' + ('\n' if not count % 16 else ' '))
